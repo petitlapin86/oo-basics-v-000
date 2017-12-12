7535dc4cb93e4f8d510551b823b4d8a7336b721b
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-12 18:54:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,10 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
-end # Make your shoe class here!
-end 
+  attr_accessor :color, :size, :material, :condition #creates reader and writer
+
+  attr_reader :brand #attribute reader 
+
+def initialize(brand)
+  @brand = brand
+end
+end # end shoe class 
+
